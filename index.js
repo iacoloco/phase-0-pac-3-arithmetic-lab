708a30053bd248849cbe6e0e4f6169daa87cc373
Iachini <iachiniarmndo@gmail.com>
2022-12-03 12:07:20 +0000

    commit tree a8e93c897e74239d5b0fd454f84c6ab36493c1ea

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b){
+return (a+b);
+}
+add(4,3);
+
+
+function subtract(a,b){
+    return (a-b);
+}
+subtract(10,10);
+    console.log(subtract(10,10));
+
+function multiply(a,b){
+    return a*b
+    }
+multiply(3,9);
+console.log(multiply(3,9));
+
+function divide(a,b){
+    return a/b
+}
+divide(100/3);
+console.log(divide(100,3));
+
+function increment(n){
+    return n+=1;
+}
+increment(2);
+console.log(increment(2));
+
+function decrement(n){
+    return n-=1;
+}
+decrement(4);
+console.log(4);
